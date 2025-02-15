4b1b67e04f14a154427c916fe2af0f60034fc350
Suetam94 <mateusviniciusdasilva@outlook.com>
2025-02-15 15:08:33 -0300

    feat: base layout

---
+++ src/components/layout/AppFooter.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-footer app color="grey darken-3" class="white--text text-center">
+    <v-container>
+      <span>&copy; {{ new Date().getFullYear() }} Gestão Acadêmica</span>
+    </v-container>
+  </v-footer>
+</template>
+
+<script lang="ts">
+export default {
+  name: "AppFooter",
+};
+</script>
---
+++ src/components/layout/AppSidebar.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-navigation-drawer v-model="isOpen" app>
+    <v-list>
+      <v-list-item to="/materias">
+        <v-icon class="mr-2">mdi-book</v-icon>
+        <v-list-item-title>Matérias</v-list-item-title>
+      </v-list-item>
+
+      <v-list-item to="/alunos">
+        <v-icon class="mr-2">mdi-account</v-icon>
+        <v-list-item-title>Alunos</v-list-item-title>
+      </v-list-item>
+
+      <v-list-item to="/provas">
+        <v-icon class="mr-2">mdi-file-document</v-icon>
+        <v-list-item-title>Provas</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from "vue";
+
+export default defineComponent({
+  name: "AppSidebar",
+  props: {
+    modelValue: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  setup(props, { emit }) {
+    const isOpen = computed({
+      get: () => props.modelValue,
+      set: (value: boolean) => emit("update:modelValue", value),
+    });
+
+    return { isOpen };
+  },
+});
+</script>
