b21ec7802bfb6fe312eab0ea96a4d3e206f43862
Suetam94 <mateusviniciusdasilva@outlook.com>
2025-02-16 18:56:48 -0300

    feat: students view api integration and improvements

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -1,7 +1,14 @@
 <template>
-  <v-app-bar app color="primary" dark>
-    <v-app-bar-title class="cursor-pointer" @click="goTo('/')">Gestão Acadêmica</v-app-bar-title>
+  <v-app-bar app color="primary" class="px-10" dark>
+    <!-- Novo container para imagem e título -->
+    <div class="d-flex align-center cursor-pointer" @click="goTo('/')">
+      <v-img src="favicon.webp" contain :width="32" :height="32" class="mr-2"></v-img>
+      <span class="title-text">Gestão Acadêmica</span>
+    </div>
+
     <v-spacer></v-spacer>
+
+    <!-- Botão do menu -->
     <v-btn icon @click="$emit('toggle-drawer')">
       <v-icon>mdi-menu</v-icon>
     </v-btn>
@@ -9,7 +16,7 @@
 </template>
 
 <script lang="ts">
-import { useRouter } from 'vue-router'
+import { useRouter } from "vue-router";
 
 export default {
   name: "AppHeader",
@@ -24,3 +31,15 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.cursor-pointer {
+  cursor: pointer;
+}
+
+/* 🔹 Garante alinhamento correto da imagem e do título */
+.title-text {
+  font-size: 18px;
+  line-height: 1;
+}
+</style>
